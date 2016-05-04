a32e261d1cf07edbdf0bc01d71ed5610091f1a7b
khlebtsovalexey <alexey.khlebtsov@i-b.com>
2016-05-04 10:30:25 +0200

    Initial Commit

---
+++ marvel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  marvel
+//
+//  Created by khlebtsov alexey on 04/05/16.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
